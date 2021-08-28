cf1ae757fe3fce9e76af8f4f0b8440409f2bfd3f
Lakith COOP <lakith.l@coopinsu.com>
2021-08-28 19:37:24 +0530

    Project File1 28/08/2021

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('home');
+});
 
 //Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
